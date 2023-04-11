e648fbc6702ab124afa0b314010cff4517c51371
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-04-11 18:52:11 +0200

    Add files via upload

---
+++ todo-list/app.js
@@ -0,0 +1,4 @@
+// Selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
